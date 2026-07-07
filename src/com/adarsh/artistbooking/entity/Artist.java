3f91269c8cf4d80bac94448b726183506131e508
Adarsh Rajput <adarshrajput2408@gmail.com>
2026-07-08 02:03:56 +0530

    Add Artist entity and Category enum

--- src/com/adarsh/artistbooking/entity/Artist.java
+++ src/com/adarsh/artistbooking/entity/Artist.java
@@ -1,6 +1,14 @@
 package com.adarsh.artistbooking.entity;
 
+import com.adarsh.artistbooking.enums.Category;
+
 public class Artist {
     private long id;
     private String name;
+    Category category;
+    private int experience;
+    private String city;
+    private double expectedamount;
+    private double rating;
+
 }
---
+++ src/com/adarsh/artistbooking/enums/Category.java
@@ -0,0 +1,13 @@
+package com.adarsh.artistbooking.enums;
+
+public enum Category {
+    SINGER,
+
+    GUITARIST,
+
+    PIANIST,
+
+    DRUMMER,
+
+    BAND
+}
